3bd512886cc137d6c58d2ee9d376ee29dfe3e1c3
senrithfernando <rihansenrithvta@gmail.com>
2026-04-21 10:58:04 +0530

    feat(frontend):create file structer

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate } from
+'react-router-dom';
+
+import './App.css'
+
+export function App() {
+  return (
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <Routes>
+ 
+      </Routes>
+    </Router>);
+
+}
+
+export default App
