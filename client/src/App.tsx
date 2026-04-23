2ebc12ff4afbd605a0095fff504d07ad80f0a482
senrithfernando <rihansenrithvta@gmail.com>
2026-04-23 11:57:30 +0530

    feat(resource): add frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,14 +7,19 @@ import {
 'react-router-dom';
 
 import './App.css'
+import { DashboardLayout } from './components/DashboardLayout';
+import { FacilitiesAssets } from './pages/FacilitiesAssets';
 
 export function App() {
   return (
     <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
- 
-      </Routes>
-    </Router>);
+        
+   <Route path="/" element={<Navigate to="/dashboard" replace />} />
+   <Route path="/facilities" element={ <DashboardLayout> <FacilitiesAssets /> </DashboardLayout> } /> 
+  
+  </Routes>
+  </Router>);
 
 }
 
